29e1cac4a8876156fe49581195575910ac07c8bb
Alan Malloy <alang@malloys.org>
2011-11-25 14:05:37 -0800

    Drop the docstrings I copied from the defprotocol.

--- src/mongo_session/core.clj
+++ src/mongo_session/core.clj
@@ -9,22 +9,15 @@
 (defrecord MongoSessionStore [coll]
   SessionStore
   (read-session [this key]
-    "Read a session map from the store. If the key is not found, an empty map
-    is returned."
     (-> (:data (fetch-one coll
                           :where {:_id key}
                           :only [:data]))
         (or {})))
   (write-session [this key data]
-    "Write a session map to the store. Returns the (possibly changed) key under
-    which the data was stored. If the key is nil, the session is considered
-    to be new, and a fresh key should be generated."
     (let [key (or key (new-key))]
       (update! coll {:_id key} {:$set {:data data}})
       key))
   (delete-session [this key]
-    "Delete a session map from the store, and returns the session key. If the
-    returned key is nil, the session cookie will be removed."
     (destroy! coll {:_id key})
     nil))
 
