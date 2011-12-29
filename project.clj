9524536a908ab19f31d2c885bf212e060cebc0f5
Anthony Grimes <i@raynes.me>
2011-12-29 17:51:46 -0600

    Add description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject amalloy/mongo-session "0.0.1"
-  :description "TODO: add summary of your project"
+  :description "A MongoDB session store for Ring. "
   :dependencies [[clojure "1.2.1"]
                  [ring "0.3.8"]
                  [congomongo "0.1.7"]])
