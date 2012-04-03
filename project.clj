be9932000cce659391bfeaf69af7dac0c3bc8d58
Alan Malloy <alan@malloys.org>
2012-04-03 15:19:33 -0700

    0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/mongo-session "0.0.1"
+(defproject amalloy/mongo-session "0.0.2"
   :description "A MongoDB session store for Ring. "
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-core "1.0.2"]
